a95a129284cbf5ac1bd2c14cb4906d0daeb7d9f9
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2017-10-10 09:12:47 -0600

    Add files via upload

---
+++ Ex5/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+    <app-header></app-header>
+        <hr>
+        <div class="row">
+           <app-server></app-server>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <div class="row">
+           <app-footer></app-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './Components/shared/Header.vue'
+    import Footer from './Components/shared/Footer.vue'
+    import ServerDetails from './Components/server/ServerDetails.vue'
+    import Server from './Components/server/Server.vue'
+    export default{
+            components: {
+                'app-header': Header,
+                'app-footer': Footer,
+                'app-server-details': ServerDetails,
+                'app-server': Server
+            }
+    }
+</script>
+
+<style>
+
+</style>
