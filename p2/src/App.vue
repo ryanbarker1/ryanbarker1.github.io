2c423d2496d9858e1484b6571f4df68d823614d5
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2017-10-22 19:31:01 -0600

    Add files via upload

---
+++ p2/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <!--ToDo: Use the Header component selector-->
+        <app-header
+                :quoteCount = "quote"
+                :maxQuotes = "maxQuotes">
+        </app-header>
+            <!--Bind data props quoteCount and set it equal to the quote length-->
+            <!--Bind data props maxQuotes and set it equal to the maxQuotes created below-->
+
+        <!--ToDo: Use the NewQuote component selector-->
+        <app-new-quote
+                @quoteAdded="newQuote"></app-new-quote>
+            <!--Initiate an $emit event listener called quoteAdded that is equal to newQuote-->
+        <!--ToDo: Use the QuoteGrid component selector-->
+        <app-quote-grid
+                    :quotes = "quotes"
+                    @quoteDeleted="deleteQuote"></app-quote-grid>
+            <!--Bind data props quotes and set it equal to the quotes array created below-->
+            <!--Call quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//  Todo: Import QuoteGrid, NewQuote, and Header Components
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+    export default {
+        data: function () {
+            return {
+                // ToDo: Create an array data object called quotes
+                    // Have at least one string quote by default
+                    quotes: [],
+                // ToDo: Create a data object maxQuotes that is set to 10
+                maxQuotes: 10,
+            }
+        },
+        methods: {
+            // ToDo: Create a method that checks when a newQuote is created and takes the parameter quote.
+            newQuote(quote){
+
+                if(this.quotes.length >= this.maxQuotes)
+                {
+                    return alert('To many delete extra quotes');
+                }
+                this.quotes.push(quote);
+            },
+                // This method will check if the quotes (Array Data Object) length is greater than or equal to the max quotes allowed
+                // If it is return an alert telling the user to delete a quote
+                // Outside of conditional, push quote (method parameter) to the quotes array
+
+            // ToDo: Create a method deleteQuote that takes in a parameter of index
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+                // This method will splice the quotes array by (index, 1)
+        },
+        components: {
+            // ToDo: Initiate the QuoteGrid, NewQuote, and Header Components with Custom Selectors
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ p2/src/components/Quote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <!--ToDo: Add default slot tag for passing the quote-->
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    /*ToDo: Create a class panel that sets the font-family to preffered font*/
+    .panel{
+        font-family: SansSerif;
+        font-size: 16px;
+        color: blue;
+    }
+        /*Set a specific front size to your liking*/
+        /*Set a color of your choice*/
+
+    /*ToDo: Create a class quote that sets the cursor to a pointer*/
+.quote{
+    cursor: pointer;
+}
+    /*ToDo: Create a hover effect on the class quote changing the background color to a color you prefer*/
+    .quote:hover{
+        background-color: grey;
+        color: white;
+    }
+</style>
