7401520cc4bf99fa3c6d0022e93eba9e27a404aa
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2017-11-02 11:25:34 -0600

    Update main.js

--- Ex8/src/main.js
+++ Ex8/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
     bind(el, binding, vnode) {
-        el.style.backgroundColor = 'blue';
+        el.style.color = 'blue';
     }
 });
 
