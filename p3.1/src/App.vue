62d854da4a0264eae77d2f37332845227c2e62f6
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2017-12-07 12:19:46 -0700

    Add files via upload

---
+++ p3.1/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container">
+        <!--ToDo: Load app-header-->
+        <app-Header></app-Header>
+        <div class="row">
+            <div class="col-xs-12">
+                <transition name="slide" mode="out-in">
+                    <!--ToDo: Load router-view-->
+                <router-view></router-view>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    //ToDo: Import Header Component
+import Header from './components/Header.vue'
+    export default {
+        //ToDo: Initialize Header component and name it appHeader
+        components: {
+            appHeader: Header
+        },
+        //ToDo: On page creation dispatch 'initStocks' to the vuex $store
+        created() {
+            this.$store.dispatch('initStocks')
+        }
+
+    }
+</script>
+<style>
+    body {
+        padding: 30px;
+    }
+    /*ToDo: Create slide-in effect for enter-active*/
+    /*ToDo: Create slide-in effect for enter-leave*/
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+    }
+    .slide-enter-leave{
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    /*ToDo: Create Keyframes for slide-in, remember to include from and to*/
+        /*You can use any settings*/
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to{
+            transform: translateY(0);
+        }
+    }
+    @keyframes slide-out {
+        from{
+            transform: translateY(0);
+        }
+        to{
+            transform: translateY(20px);
+        }
+    }
+    /*ToDo: Create Keyframes for slide-out, remember to include from and to*/
+        /*You can use any settings*/
+
+</style>
+
---
+++ p3.1/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Trade or View your Portfolio</h1>
+        <h6>You may Save & Load your Data</h6>
+        <h6>Click on 'End Day' to begin a New Day!</h6>
+        <hr>
+
+        <p>Your Funds: {{ funds | currency }}
+            <!--ToDo: Call funds computed function and pipe the currency filter that is created in main.js-->
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            //ToDo: Create a computed function called funds
+                //ToDo: Have funds() return this.$store.getters.funds
+            funds(){
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
