cf272dc6d94fceca49079517e31b3b35277a0d43
ryanbarker1 <31492268+ryanbarker1@users.noreply.github.com>
2017-12-07 08:44:57 -0700

    Add files via upload

---
+++ p3-1/src/components/stocks/Stocks.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!--ToDo: Load app-stock component-->
+            <!--ToDo: Loop through stock in stocks using v-for-->
+            <!--ToDo: Bind to stock using : and pass stock from the v-for-->
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+    //ToDo: Import Stock from ./Stock.vue
+import Stock from './Stock.vue'
+    export default {
+        //ToDo: Initialize Stock component and name it appStock
+            components: {
+                appStock: Stock
+            },
+        computed: {
+            //ToDo: Create stocks computer method
+                // ToDo: Return $store.getters.stocks
+            stocks(){
+                return $store.getters.stocks
+            }
+        }
+    }
+</script>
